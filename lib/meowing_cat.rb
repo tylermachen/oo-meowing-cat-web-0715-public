ffa9bb3f827efb44522db8d14dbfba8a30735273
Tyler Machen <tylermachen@gmail.com>
2015-08-04 13:55:54 -0400

    complete Cat class and Box bonus class

---
+++ lib/meowing_cat.rb
@@ -0,0 +1,25 @@
+require_relative './box.rb'
+
+class Cat
+  attr_accessor :name, :size
+
+  def meow
+    puts "meow!"
+  end
+
+  def get_in_box(box)
+    if self.size <= box.size
+      puts "Meow! I'm in the box"
+    else
+      puts "Sad meow, I'm not in box"
+    end
+  end
+end
+
+maru = Cat.new
+maru.size = 2
+
+fedex_box = Box.new
+fedex_box.size = 3
+
+maru.get_in_box(fedex_box)
